4f9d4f62babdadf6b048d571cea62bf35f60e20b
nobodykid <muhamadiqbal@live.co.uk>
2016-05-05 09:45:52 +0700

    add solutions

---
+++ CollectionMvc/CollectionMvc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CollectionMvc.Models;
+
+namespace CollectionMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Participant> participants = new List<Participant>()
+            {
+                new Participant() {PersonId=1, Name="Muhamad Iqbal", Region="Bodeta" },
+                new Participant() {PersonId=2, Name="Ade Haryanto", Region="Sumatera" }
+            };
+
+            return View(participants);
+        }
+    }
+}
---
+++ CollectionMvc/CollectionMvc/Models/Participant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionMvc.Models
+{
+    public class Participant
+    {
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Region { get; set; }
+    }
+}
