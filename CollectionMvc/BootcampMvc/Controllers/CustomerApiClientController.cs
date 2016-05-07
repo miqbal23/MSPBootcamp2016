2d356ac80726606c9e9f6e7476bdd34cecd30be2
nobodykid <muhamadiqbal@live.co.uk>
2016-05-07 16:24:31 +0700

    created Controller and View for Web API

---
+++ CollectionMvc/BootcampMvc/Controllers/CustomerApiClientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootcampMvc.Controllers
+{
+    public class CustomerApiClientController : Controller
+    {
+        // GET: CustomerApiClient
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
