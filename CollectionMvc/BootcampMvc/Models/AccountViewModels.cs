6e0090a85ff3dfd81439bdafe2da95850d0b9b87
nobodykid <muhamadiqbal@live.co.uk>
2016-05-07 10:27:42 +0700

    modified Account and generated Configuration and SQLQuery for database-first model creation

--- CollectionMvc/BootcampMvc/Models/AccountViewModels.cs
+++ CollectionMvc/BootcampMvc/Models/AccountViewModels.cs
@@ -49,9 +49,13 @@ namespace BootcampMvc.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        //[Required]
+        //[Display(Name = "Email")]
+        //[EmailAddress]
+        //public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -65,12 +69,16 @@ namespace BootcampMvc.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
